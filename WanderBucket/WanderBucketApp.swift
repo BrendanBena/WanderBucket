ec1dbec0d6f493c5a0f9297673719f3dff1f3fc8
BrendanBena <bzb98@live.com>
2021-02-02 11:30:07 -0600

    Initial Commit

---
+++ WanderBucket/WanderBucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderBucketApp.swift
+//  WanderBucket
+//
+//  Created by Brendan Bena on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WanderBucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
