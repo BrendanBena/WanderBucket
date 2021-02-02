715fe462dcadcc0863bb9565bf3423f00af5f3ce
BrendanBena <bzb98@live.com>
2021-02-02 16:19:36 -0600

    Updates to login in screen

--- WanderBucket/ContentView.swift
+++ WanderBucket/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         VStack {
             Image("globe")
@@ -20,15 +24,48 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .multilineTextAlignment(.center)
-                .padding(10.0)
+                .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
+                
 
             Text("Not All Those Who Wander Are Lost...")
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundColor(Color.white)
                 .multilineTextAlignment(.center)
+                .padding(.vertical, 5.0)
+            
+            TextField("Username", text: $username)
+                .padding()
+                .colorInvert()
+                .background(Color.white)
+                .cornerRadius(5.0)
+                .padding(.horizontal, 35.0)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .colorInvert()
+                .background(Color.white)
+                .cornerRadius(5.0)
+                .padding(.horizontal, 35.0)
+            
+            Text("LOGIN")
+                .font(.headline)
+                .foregroundColor(.black)
+                .frame(width: 150, height: 50)
+                .background(Color.white)
+                .cornerRadius(15.0)
+            
+            Text("Forgot Password? Click Here.")
+                .font(.footnote)
+                .padding(.top, 10.0)
+            
+            Text("Don't Have an Account? Sign Up.")
+                .font(.footnote)
+                .padding(.top, 5.0)
+                
             
         }
+        .padding()
     }
 }
 
