05f3a15778e634e66f856728d7a7360380e76637
BrendanBena <bzb98@live.com>
2021-02-03 18:25:22 -0600

    Added sign up page

---
+++ WanderBucket/SignUpScreen.swift
@@ -0,0 +1,207 @@
+//
+//  SignUpScreen.swift
+//  WanderBucket
+//
+//  Created by Brendan Bena on 2/3/21.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var confirmedPassword: String = ""
+    @State var dob: String = ""
+    @State var nationality: String = ""
+    @State var email: String = ""
+    @State var gender: String = ""
+    
+    var body: some View {
+        VStack {
+            SignUpTitle()
+            //AppLogo()
+            UsernameField(username: $username)
+            EmailField(email: $email)
+            PasswordField(password: $password)
+            ComfirmPasswordField(confirmedPassword: $confirmedPassword)
+            DOBField(dob: $dob)
+            NationalityField(nationality: $nationality)
+            GenderField(gender: $gender)
+            Button(action: {print("Sign Up Button tapped")}) {
+                FirstSignUpButton()
+            }
+        }
+        .padding()
+    }
+}
+
+// Password Validation Function
+// Username Validation Function
+// Email Validation Function
+// DOB Validation Function
+struct SignUpTitle : View {
+    var body : some View {
+        VStack {
+            
+            Text("WanderBucket")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding(/*@START_MENU_TOKEN@*/.horizontal, 15.0/*@END_MENU_TOKEN@*/)
+            Text("Create An Account:")
+                .font(.title2)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+                .padding(/*@START_MENU_TOKEN@*/.all, 15.0/*@END_MENU_TOKEN@*/)
+            
+        }
+    }
+}
+
+struct EmailField: View {
+
+    @Binding var email: String
+    var body : some View {
+        TextField("Email", text: $email)
+            .padding()
+            .colorInvert()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.horizontal, 40.0)
+            .padding(.vertical, 5.0)
+    }
+}
+
+struct ComfirmPasswordField: View {
+
+    @Binding var confirmedPassword: String
+    var body : some View {
+        TextField("Comfirm Password", text: $confirmedPassword)
+            .padding()
+            .colorInvert()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.horizontal, 40.0)
+            .padding(.vertical, 5.0)
+    }
+}
+
+// Think about calendar here
+struct DOBField: View {
+
+    @Binding var dob: String
+    var body : some View {
+        TextField("MM/DD/YYYY", text: $dob)
+            .padding()
+            .colorInvert()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            //.datePickerStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Date Picker Style@*/DefaultDatePickerStyle()/*@END_MENU_TOKEN@*/)
+            .padding(.horizontal, 40.0)
+            .padding(.vertical, 5.0)
+    }
+}
+
+// Think about country selection instead
+struct NationalityField: View {
+
+    @Binding var nationality: String
+    var body : some View {
+        TextField("Nationality (Optional)", text: $nationality)
+            .padding()
+            .colorInvert()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.horizontal, 40.0)
+            .padding(.vertical, 5.0)
+    }
+}
+
+// Maybe take out?
+struct GenderField: View {
+
+    @Binding var gender: String
+    var body : some View {
+        TextField("Gender (Optional)", text: $gender)
+            .padding()
+            .colorInvert()
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.horizontal, 40.0)
+            .padding(.vertical, 5.0)
+    }
+}
+
+struct FirstSignUpButton : View {
+    var body : some View {
+        Text("Sign Up!")
+            .font(.headline)
+            .foregroundColor(.black)
+            .frame(width: 235, height: 45)
+            .background(Color.white)
+            .cornerRadius(15.0)
+    }
+}
+
+
+struct SignUpScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SignUpScreen()
+                .preferredColorScheme(.dark)
+                .previewLayout(.device)
+        }
+    }
+}
+
+
+
+
+
+//struct AppTitle : View {
+//    var body : some View {
+//        Text("WanderBucket")
+//            .font(.largeTitle)
+//            .fontWeight(.bold)
+//            .multilineTextAlignment(.center)
+//            .padding(/*@START_MENU_TOKEN@*/.all, 15.0/*@END_MENU_TOKEN@*/)
+//    }
+//}
+
+//struct AppLogo : View {
+//    var body : some View {
+//        Image("globe")
+//            .padding(-22.0)
+//            .clipShape(Circle())
+//            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+//            .shadow(radius: 10)
+//    }
+//}
+//
+//
+//struct UsernameField: View {
+//
+//    @Binding var username: String
+//    var body : some View {
+//        TextField("Username", text: $username)
+//            .padding()
+//            .colorInvert()
+//            .background(Color.white)
+//            .cornerRadius(10.0)
+//            .padding(.horizontal, 40.0)
+//    }
+//}
+//
+//struct PasswordField: View {
+//
+//    @Binding var password: String
+//    var body: some View {
+//        SecureField("Password", text: $password)
+//            .padding()
+//            .colorInvert()
+//            .background(Color.white)
+//            .cornerRadius(10.0)
+//            .padding(.horizontal, 40.0)
+//    }
+//}
